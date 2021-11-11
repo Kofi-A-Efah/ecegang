d74d729b193b48760c486ed0855f444f59d74145
Kofi Efah <54334161+Kofi-A-Efah@users.noreply.github.com>
2021-11-10 21:09:02 -0500

    Add files via upload

---
+++ TKbuttons/port_expander2_brl4.c
@@ -0,0 +1,184 @@
+#include "port_expander2_brl4.h"
+
+#define SET_CS    {mPORTBSetBits(BIT_9);}
+#define CLEAR_CS  {mPORTBClearBits(BIT_9);}
+
+// === spi bit widths ====================================================
+// hit the SPI control register directly, SPI2
+// Change the SPI bit modes on the fly, mid-transaction if necessary
+inline void SPI_Mode16(void){  // configure SPI2 for 16-bit mode
+    SPI2CONSET = 0x400;
+    SPI2CONCLR = 0x800;
+}
+// ========
+inline void SPI_Mode8(void){  // configure SPI2 for 8-bit mode
+    SPI2CONCLR = 0x400;
+    SPI2CONCLR = 0x800;
+}
+// ========
+inline void SPI_Mode32(void){  // configure SPI2 for 32-bit mode
+    SPI2CONCLR = 0x400;
+    SPI2CONSET = 0x800;
+}
+
+void initPE() {
+  // control CS for DAC
+  volatile SpiChannel pe_spi = SPI_CHANNEL1;
+  volatile int spiClkDiv = 4; // 10 MHz max speed for this DAC
+  mPORTBSetPinsDigitalOut(BIT_9); // use RPB9 (pin 21)
+  mPORTBSetBits(BIT_9); // CS active low
+  PPSOutput(2, RPB5, SDO2); // use RPB5 (pin 14) for SDO2
+  PPSInput(3, SDI2,RPA4); // SDI2
+  
+  SpiChnOpen(pe_spi, SPI_OPEN_ON | SPI_OPEN_MODE8 | SPI_OPEN_MSTEN | SPI_OPEN_CKE_REV, spiClkDiv);
+  
+  writePE(IOCON, ( CLEAR_BANK   | CLEAR_MIRROR | SET_SEQOP |
+                   CLEAR_DISSLW | CLEAR_HAEN   | CLEAR_ODR |
+                   CLEAR_INTPOL ));
+}
+
+void clearBits(unsigned char addr, unsigned char bitmask){
+  if (addr <= 0x15){
+    unsigned char cur_val = readPE(addr);
+    writePE(addr, cur_val & (~bitmask));
+  }
+}
+
+void setBits(unsigned char addr, unsigned char bitmask){
+  if (addr <= 0x15){
+    unsigned char cur_val = readPE(addr);
+    writePE(addr, cur_val | (bitmask));
+  }
+}
+
+void toggleBits(unsigned char addr, unsigned char bitmask){
+  if (addr <= 0x15){
+    unsigned char cur_val = readPE(addr);
+    writePE(addr, cur_val ^ (bitmask));
+  }
+}
+
+unsigned char readBits(unsigned char addr, unsigned char bitmask){
+  if (addr <= 0x15){
+    unsigned char cur_val = readPE(addr) & bitmask ;
+    return cur_val ;
+  }
+}
+
+void mPortYYSetPinsOut(unsigned char bitmask){
+  clearBits(IODIRYY, bitmask);
+}
+
+void mPortZZSetPinsOut(unsigned char bitmask){
+  clearBits(IODIRZZ, bitmask);
+}
+
+void mPortYYSetPinsIn(unsigned char bitmask){
+  setBits(IODIRYY, bitmask);
+}
+
+void mPortZZSetPinsIn(unsigned char bitmask){
+  setBits(IODIRZZ, bitmask);
+}
+
+void mPortYYIntEnable(unsigned char bitmask){
+  setBits(GPINTENYY, bitmask);
+}
+
+void mPortZZIntEnable(unsigned char bitmask){
+  setBits(GPINTENZZ, bitmask);
+}
+
+void mPortYYIntDisable(unsigned char bitmask){
+  clearBits(GPINTENYY, bitmask);
+}
+
+void mPortZZIntDisable(unsigned char bitmask){
+  clearBits(GPINTENZZ, bitmask);
+}
+
+void mPortYYEnablePullUp(unsigned char bitmask){
+  setBits(GPPUYY, bitmask);
+}
+
+void mPortZZEnablePullUp(unsigned char bitmask){
+  setBits(GPPUZZ, bitmask);
+}
+
+void mPortYYDisablePullUp(unsigned char bitmask){
+  clearBits(GPPUYY, bitmask);
+}
+
+void mPortZZDisablePullUp(unsigned char bitmask){
+  clearBits(GPPUZZ, bitmask);
+}
+
+inline void writePE(unsigned char reg_addr, unsigned char data) {
+  unsigned char junk = 0;
+   
+  // test for ready
+  while (TxBufFullSPI1());
+  
+  // CS low to start transaction
+  CLEAR_CS
+  // 8-bits
+  SPI_Mode8();
+  // OPCODE and HW Address (Should always be 0b0100000), set LSB for write
+  WriteSPI1((PE_OPCODE_HEADER | WRITE));
+  // test for done
+  while (SPI1STATbits.SPIBUSY); // wait for byte to be sent
+  junk = ReadSPI1();
+  // Input Register Address
+
+  WriteSPI1(reg_addr);
+  while (SPI1STATbits.SPIBUSY); // wait for byte to be sent
+  junk = ReadSPI1();
+  // One byte of data to write to register
+  
+  WriteSPI1(data);
+  // test for done
+  while (SPI1STATbits.SPIBUSY); // wait for end of transaction
+  junk = ReadSPI1();
+  // CS high
+  SET_CS
+  
+}
+
+inline unsigned char readPE(unsigned char reg_addr) {
+  unsigned char out = 0;
+  
+  // test for ready
+  while (TxBufFullSPI1());
+  
+  // CS low to start transaction
+  CLEAR_CS
+  
+  // 8-bits
+  SPI_Mode8();
+  // OPCODE and HW Address (Should always be 0b0100000), clear LSB for write
+  WriteSPI1((PE_OPCODE_HEADER | READ));
+  // test for done
+  while (SPI1STATbits.SPIBUSY); // wait for byte to be sent
+  out = ReadSPI1(); //junk
+  // Input Register Address
+  // test for ready
+  while (TxBufFullSPI1());
+  // 8-bits
+  
+  WriteSPI2(reg_addr);
+  while (SPI1STATbits.SPIBUSY); // wait for byte to be sent
+  out = ReadSPI1(); // junk
+  // One byte of dummy data to write to register
+  // test for ready
+  while (TxBufFullSPI1());
+  // 8-bits
+  
+  WriteSPI1(out);
+  // test for done
+  while (SPI1STATbits.SPIBUSY); // wait for end of transaction
+  out = ReadSPI1(); // bingo
+  // CS high
+  SET_CS
+  
+  return out;
+}
---
+++ TKbuttons/port_expander2_brl4.h
@@ -0,0 +1,106 @@
+/* 
+ * File:   port_expander2.h
+ * Author: Sean edited by kofi
+ *
+ * Created on October 3, 2016, 10:28 AM
+ */
+
+#ifndef PORT_EXPANDER2_H
+#define	PORT_EXPANDER2_H
+/* Library for interacting with MCP23S17 port expander */
+#include "plib.h"
+
+#define PE_OPCODE_HEADER 0b01000000
+#define READ 0b00000001
+#define WRITE 0b00000000
+
+// IOCON Settings
+#define SET_BANK     0x80
+#define CLEAR_BANK   0x00
+#define SET_MIRROR   0x40
+#define CLEAR_MIRROR 0x00
+#define SET_SEQOP    0x20
+#define CLEAR_SEQOP  0x00
+#define SET_DISSLW   0x10
+#define CLEAR_DISSLW 0x00
+#define SET_HAEN     0x08
+#define CLEAR_HAEN   0x00
+#define SET_ODR      0x04
+#define CLEAR_ODR    0x00
+#define SET_INTPOL   0x02
+#define CLEAR_INTPOL 0x00
+
+
+// **** Register Addresses (BANK=0) ****
+// Note: Rename all PortA to PortYY, PortB to PortZZ to avoid confusion with PIC
+#define IODIRYY   0x00
+#define IODIRZZ   0x01
+#define IPOLYY    0x02
+#define IPOLZZ    0x03
+#define GPINTENYY 0x04
+#define GPINTENZZ 0x05
+#define DEFVALYY  0x06
+#define DEFVALZZ  0x07
+#define INTCONYY  0x08
+#define INTCONZZ  0x09
+#define IOCON    0x0A
+//#define IOCON    0x0B
+#define GPPUYY    0x0C
+#define GPPUZZ    0x0D
+#define INTFYY    0x0E
+#define INTFZZ    0x0F
+#define INTCAPYY  0x10
+#define INTCAPZZ  0x11
+#define GPIOYY    0x12
+#define GPIOZZ    0x13
+#define OLATYY    0x14
+#define OLATZZ    0x15
+
+/* Initializes SPI channel 2 with 8-bit modeRB9 for CS: 
+ * pins used:
+ *  --  CS - RPB9 (Pin 18)
+ *  -- SCK - SCK1 (Pin 25) // RPB14
+ *  -- SDI - RPB2 (Pin  6) We'll figure it out
+ *  -- SDO - RPB5 (Pin 14) We'll figure it out
+ * Sets the clock divisor for pb_clock to 4, giving 10MHz (fastest possible for
+ * our MCP23S17 package)
+ */
+void initPE();
+
+void mPortYYSetPinsOut(unsigned char);
+
+void mPortZZSetPinsOut(unsigned char);
+
+void mPortYYSetPinsIn(unsigned char);
+
+void mPortZZSetPinsIn(unsigned char);
+
+void mPortYYIntEnable(unsigned char);
+
+void mPortYYIntDisable(unsigned char);
+
+void mPortZZIntEnable(unsigned char);
+
+void mPortZZIntDisable(unsigned char);
+
+void mPortYYEnablePullUp(unsigned char);
+
+void mPortZZEnablePullUp(unsigned char);
+
+void mPortYYDisablePullUp(unsigned char);
+
+void mPortZZDisablePullUp(unsigned char);
+
+
+/* Takes a register address on port expander and a data byte, and writes the 
+ * data to the target register. */
+inline void writePE(unsigned char, unsigned char);
+
+/* Takes a register address on port expander and returns the data byte from that
+ * target register. */
+inline unsigned char readPE(unsigned char);
+
+// spi2 lock so that port expander OR DAC can use the channel
+extern volatile int spi1_lock ;
+#endif	/* PORT_EXPANDER_H */
+
